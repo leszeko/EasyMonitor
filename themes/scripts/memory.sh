ba32fa185825d8c8df90d7a6b08b475be8ad969c
leszeko <easy.monitor.theme@gmail.com>
2018-04-02 04:46:21 +0200

    Deleting files

--- themes/scripts/memory.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# memory.sh script
-
-sudo /usr/sbin/dmidecode --type 17 | grep 'Size' | awk '{print NR":",$2,$3;}' | awk 'BEGIN {RS="\n\n"; FS="\n";} {print $1,$2,$3,$4;}'
--- themes/scripts/netstat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# netstat.sh script
-
-watch 'netstat -r; netstat |grep tcp'
